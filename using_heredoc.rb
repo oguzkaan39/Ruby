d3c0b83dcada9a601887e732b604f38cc3f246a5
oguzkaan39 <oguz_kaan_gunduz@hotmail.com>
2015-11-25 15:55:09 +0200

    using heredoc

---
+++ using_heredoc.rb
@@ -0,0 +1,15 @@
+#uncoding: utf-8
+hd = <<SINIRLAYICI
+
+Bu bir "heredoc" kullanım örneğidir.
+İçerisinde "" leri rahatca kullanabiliriz.
+Fakat #{} süslü parantezle kod gömme işlemi yapamayız.
+Bir de burada yazıyı nereye yazarsak bize öyle gözükecektir, 
+yani boşluklar önemlidir. Örneğin :
+
+Merhaba 
+            Dünya 
+                      :)
+
+SINIRLAYICI
+puts hd
