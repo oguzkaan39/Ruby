cae61fd9c19e0597a37ec62cdd2989bb03e37784
oguzkaan39 <oguz_kaan_gunduz@hotmail.com>
2015-11-25 15:51:08 +0200

    using heredoc

--- class_example.rb
+++ class_example.rb
@@ -1,13 +1,2 @@
-class Bitkiler 
-	attr_accessor :bitki1, :bitki2
-	def initialize(bitki1, bitki2)
-		@bitki1 = bitki1
-		@bitki2 = bitki2
-	end
-	def bitki
-		puts "Bitkilerim #{bitki1} ve #{bitki2}"
-	end
-end
-
-bitki_sinifi = Bitkiler.new("Papatya", "Gül")
-bitki_sinifi.bitki
+metin = String.new ("Ruby de metin kavramını öğreniyorum")
+print metin
