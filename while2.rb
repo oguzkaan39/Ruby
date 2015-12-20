e2c92c80c39ad52954599f7e25080817112b535f
oguzkaan39 <oguz_kaan_gunduz@hotmail.com>
2015-12-21 01:32:28 +0200

    while2

---
+++ while2.rb
@@ -0,0 +1,28 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+
+puts "1.Toplama"
+puts "2.Çarpma "
+puts "0.Çıkış"
+
+print "Bir secim yapınız : "
+
+secim = gets.chomp
+
+while secim.to_i!=0
+
+	print "Bir seçim yapınız :"
+
+	secim=gets.chomp.to_i
+
+	if secim.to_i==1
+		puts "Toplama işlemini seçtiniz."
+	elsif secim.to_i==2
+		puts "Çarpma işlemini seçtiniz. "
+	else 
+		puts " "
+	end
+
+end
+
+
