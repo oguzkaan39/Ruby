938d06ba1338b9ef218d54dbd479a9e01d57fee3
oguzkaan39 <oguz_kaan_gunduz@hotmail.com>
2016-01-11 11:28:22 +0200

    using each

---
+++ array1.rb
@@ -0,0 +1,25 @@
+#!/usr/bin/env ruby
+# encoding: utf-8
+=begin
+dizi = %w(ruby rails sinatra)
+
+#0 indeks değerini değiştirme
+
+dizi[0] = "RUBY"
+
+puts dizi
+=end 
+
+dizi = %w(Ruby Python C Java Rails Php)
+
+ dizi.each do |eleman|
+ 	print eleman + "*"
+ end
+
+
+
+
+
+
+
+
